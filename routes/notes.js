48e9e1a972a91b28686027789da4a653b402e4c8
jatin251212 <159692753+jatin251212@users.noreply.github.com>
2024-02-26 11:08:59 +0530

    Adding update notes feature.

--- routes/notes.js
+++ routes/notes.js
@@ -46,4 +46,36 @@ router.post('/addnote',fetchuser,[
     
 });
 
+// ROUTE 3:Update am existing  note using: POST "/api/notes/updatenote". login required
+router.put('/updatenote/:id',fetchuser, async (req,res)=>{
+    try {
+        const {title, description, tag}=req.body;
+        const newNote={};
+        if(title){newNote.title=title};
+        if(description){newNote.description=description};
+        if(tag){newNote.tag=tag};
+
+        let notes=await Notes.findById(req.params.id);
+        if(!notes){
+            return res.status(404).send("Not Found");
+
+        }
+
+        if(notes.user.toString()!== req.user.id){
+            return res.status(401).send("Not Allowed");
+
+        }
+        console.log(notes.user);
+        console.log(req.user.id);
+
+        notes=await Notes.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true});
+        res.json({notes});
+ 
+    } catch (error) {
+        res.status(500).json({ error:"Internal Server Error" });
+      }
+
+});
+
+
 module.exports=router
